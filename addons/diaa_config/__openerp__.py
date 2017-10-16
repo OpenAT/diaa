2cc3349eac1a81405774d609133c79efaf386ea1
Michael Karrer <michaelkarrer81@gmail.com>
2017-10-16 13:51:33 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/diaa_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'diaa_config',
+    'summary': '''FS-Online diaa instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance diaa
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
